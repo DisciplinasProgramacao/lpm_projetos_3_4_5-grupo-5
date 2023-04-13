04770687cf2d8a901e738b1b2bbae6e0c281235f
Pedro Talma Toledo <pedrotoledo1717@gmail.com>
2023-04-12 22:18:21 -0300

    Upload - Create Class Seria
    Criada classe Serie

---
+++ codigo/Serie.java
@@ -0,0 +1,65 @@
+package codigo;
+
+public class Serie {
+    private static final String[] GENEROS = {"comedia", "romance", "terror"};
+    private String nome;
+    private String genero;
+    private String idioma;
+    private int qtdEpisodios;
+    private int audiencia;
+
+    //getters
+    public String getNome() {
+        return nome;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public String getIdioma() {
+        return idioma;
+    }
+    public int getQtdEpisodios() {
+        return qtdEpisodios;
+    }
+    public int getAudiencia() {
+        return audiencia;
+    }
+
+
+    /**
+     * constructor
+     * @param nome
+     * @param genero
+     * @param idioma
+     * @param qtdEpisodios
+     * @param audiencia
+     */
+    public Serie(String nome, String genero, String idioma, int qtdEpisodios, int audiencia) {
+        verificaGenero(genero);
+        this.nome = nome;
+        this.genero = genero;
+        this.idioma = idioma;
+        this.qtdEpisodios = qtdEpisodios;
+        this.audiencia = audiencia;
+    }
+
+    /**
+     * verifica se genero existe
+     * @param genero
+     */
+    private void verificaGenero(String genero) {
+        for(String g: GENEROS){
+            if(genero.equals(g)){
+                this.genero = genero;
+            }
+        }
+    }
+
+
+    /**
+     * aumenta audiencia em +1 para cada vez que o cliente assiste
+     */
+    public void registrarAudiencia(){
+        this.audiencia++;
+    }
+}
---
+++ codigo/SerieTest.java
@@ -0,0 +1,22 @@
+package codigo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SerieTest {
+
+
+    Serie s1;
+    @BeforeEach
+    public void prepare(){
+        s1 = new Serie("nome", "terror", "portugues", 2,10);
+
+    }
+    @Test
+    public void deveRegistrarAudiencia(){
+        s1.registrarAudiencia();
+        assertEquals(1, s1.getAudiencia());
+    }
+}
