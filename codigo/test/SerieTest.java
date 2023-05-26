0335a52c2af7905a6fc6a9a9ebead7ed5997b26a
Breno <brenorosaalmeida@yahoo.com.br>
2023-05-26 20:59:08 -0300

    Testes da classe Serie adicionados.

--- codigo/test/SerieTest.java
+++ codigo/test/SerieTest.java
@@ -1,16 +1,22 @@
 package codigo.test;
 
 import codigo.app.Serie;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SerieTest {
     Serie s1;
+    Serie s2;
     @BeforeEach
     public void prepare(){
         s1 = new Serie(1,"nome", "terror", "portugues", "2",6);
+        s2 = new Serie(2, "F.R.I.E.N.D.S", "comedia", "inglês", "22/09/1994", 236);
     }
 
     @Test
@@ -18,4 +24,33 @@ public class SerieTest {
         s1.registrarAudiencia();
         assertEquals(1, s1.getAudiencia());
     }
+
+    @Test
+    void testToString() {
+        String expected = "S;2;F.R.I.E.N.D.S;comedia;inglês;22/09/1994;236";
+        Assertions.assertEquals(expected, s2.toString());
+    }
+
+    @Test
+    void testDefinirTipoMidia() {
+        Assertions.assertEquals("S", s2.definirTipoMidia());
+    }
+
+    @Test
+    void testPegarQtdEpisodios() {
+        Assertions.assertEquals(236, s2.getQtdEpisodios());
+    }
+
+    @Test
+    void testFiltrarPorQtdEpisodiosVerdadeiro() {
+        Assertions.assertTrue(s1.filtrarPorQtdEpisodios(6));
+        Assertions.assertTrue(s2.filtrarPorQtdEpisodios(236));
+    }
+
+    @Test
+    void testFiltrarPorQtdEpisodiosFalso() {
+        Assertions.assertFalse(s1.filtrarPorQtdEpisodios(12));
+        Assertions.assertFalse(s2.filtrarPorQtdEpisodios(8));
+    }
+
 }
