72bd9786680f00ad46cc88c10aa41863fa7deb1e
Breno <brenorosaalmeida@yahoo.com.br>
2023-06-21 15:39:27 -0300

    Adicionado testes que estavam faltando e correção

--- codigo/test/ClienteTest.java
+++ codigo/test/ClienteTest.java
@@ -24,10 +24,12 @@ public class ClienteTest {
         serieDRAMA2 = new Serie(2,"SerieDRAMA2", Genero.DRAMA, Idioma.INGLES, "12", EstadoMidia.MODIFICAVEL, 58);
         serieComedia = new Serie(3,"SerieComedia", Genero.COMEDIA, Idioma.PORTUGUES, "10", EstadoMidia.MODIFICAVEL, 10);
         serieComedia2 = new Serie(4,"SerieComedia2", Genero.COMEDIA, Idioma.PORTUGUES, "10", EstadoMidia.MODIFICAVEL, 10);
-        serieComedia3 = new Serie(5,"SerieComedia3", Genero.COMEDIA, Idioma.PORTUGUES, "10", EstadoMidia.MODIFICAVEL, 10);
+        serieComedia3 = new Serie(5,"SerieComedia3", Genero.COMEDIA, Idioma.PORTUGUES, "10", EstadoMidia.MODIFICAVEL, 30);
         clienteTeste.adicionarNaLista(serieDRAMA1);
         clienteTeste.adicionarNaLista(serieDRAMA2);
         clienteTeste.adicionarNaLista(serieComedia);
+        clienteTeste.adicionarNaLista(serieComedia2);
+        clienteTeste.adicionarNaLista(serieComedia3);
 
         s1 = new Serie(6, "F.R.I.E.N.D.S", Genero.COMEDIA, Idioma.INGLES, "22/09/1994",  EstadoMidia.MODIFICAVEL, 236);
         s2 = new Serie(7, "Stranger Things", Genero.DRAMA, Idioma.INGLES, "15/06/2016",  EstadoMidia.MODIFICAVEL, 38);
@@ -38,10 +40,22 @@ public class ClienteTest {
     }
 
     @Test
+    public void testValidaParametrosConstrutor() {
+        Cliente cliente = new Cliente("NomeDeUsuario", "login", "senha");
+        assertTrue(cliente.validaParametrosConstrutor("NomeDeUsuario", "login", "senha"));
+        assertFalse(cliente.validaParametrosConstrutor("NomeDeUsuario", "loginloginloginloginlogin", "senha"));
+        assertFalse(cliente.validaParametrosConstrutor("NomeDeUsuarioNomeDeUsuarioNomeDeUsuario", "login", "senha"));
+        assertFalse(cliente.validaParametrosConstrutor("NomeDeUsuario", "login", "senhasenhasenha"));
+        assertFalse(cliente.validaParametrosConstrutor("", "login", "senha"));
+        assertFalse(cliente.validaParametrosConstrutor("NomeDeUsuario", "", "senha"));
+        assertFalse(cliente.validaParametrosConstrutor("NomeDeUsuario", "login", ""));
+    }
+
+    @Test
     public void deveAdicionarSerie(){
         Serie serieDRAMA3 = new Serie(4,"SerieDRAMA3", Genero.DRAMA, Idioma.PORTUGUES, "10", EstadoMidia.MODIFICAVEL, 5);
         clienteTeste.adicionarNaLista(serieDRAMA3);
-        assertEquals(4, clienteTeste.getListaParaVer().size());
+        assertEquals(6, clienteTeste.getListaParaVer().size());
     }
 
     @Test
@@ -52,22 +66,24 @@ public class ClienteTest {
     @Test
     public void deveRetirarSerie(){
         clienteTeste.retirarNaLista("SerieDRAMA1");
-        assertEquals(2, clienteTeste.getListaParaVer().size());
+        assertEquals(4, clienteTeste.getListaParaVer().size());
     }
 
     @Test
     public void deveFiltrarGenero(){
         assertEquals(4, clienteTeste.filtrarPorGenero(Genero.DRAMA).size());
+        assertEquals(4, clienteTeste.filtrarPorGenero(Genero.COMEDIA).size());
     }
 
     @Test
     public void deveFiltrarIdioma(){
-        assertEquals(2, clienteTeste.filtrarPorIdioma(Idioma.PORTUGUES).size());
+        assertEquals(4, clienteTeste.filtrarPorIdioma(Idioma.PORTUGUES).size());
+        assertEquals(4, clienteTeste.filtrarPorIdioma(Idioma.INGLES).size());
     }
 
     @Test
     public void deveFiltrarEpisodio(){
-        assertEquals(2, clienteTeste.filtrarPorQtdEpisodios(10).size());
+        assertEquals(3, clienteTeste.filtrarPorQtdEpisodios(10).size());
     }
 
     @Test
